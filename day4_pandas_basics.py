5dd67d8990e390eb243b8c9692953cc6db8d2828
AiswaryaRJ <aiswaryarosejacob@gmail.com>
2026-05-31 00:03:59 +0530

    Day 4: Initialized Pandas Series and multi-column DataFrames

--- day4_pandas_basics.py
+++ day4_pandas_basics.py
@@ -5,24 +5,14 @@ print('1. Labeled Pandas Series: ')
 print(altitudes_series)
 print("-"*40)
 telemetry_data = {
-
     "Battery_Pct":[98, 85, 72, 95],
-
     "Velocity_kmh": [45.2, 61.8, 55.0, 32.1],
-
     "Status": ["Optimal", "Warning", "Optimal", "Safe"]
-
 }
-
 df = pd.DataFrame(telemetry_data, index=["A", "B", "C", "D"])
-
 print("2. Formatted pandas DataFrame: ")
-
 print(df)
-
 print("-"*40)
-
 print("Executing the Velocity Column (Returns a Series): ")
-
 print(df["Velocity_kmh"])
 print("-"*40)
