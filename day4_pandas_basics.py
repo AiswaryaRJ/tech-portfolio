57c9ef06686787f0fd71e79d2dbea13cb59ceb33
AiswaryaRJ <aiswaryarosejacob@gmail.com>
2026-05-31 00:01:13 +0530

    Update day4_pandas_basics.py

--- day4_pandas_basics.py
+++ day4_pandas_basics.py
@@ -4,3 +4,21 @@ altitudes_series = pd.Series(altitudes, name="Altitude_m")
 print('1. Labeled Pandas Series: ')
 print(altitudes_series)
 print("-"*40)
+telemetry_data = {
+
+    "Battery_Pct":[98, 85, 72, 95],
+
+    "Velocity_kmh": [45.2, 61.8, 55.0, 32.1],
+
+    "Status": ["Optimal", "Warning", "Optimal", "Safe"]
+
+}
+
+df = pd.DataFrame(telemetry_data)
+
+print("2. Formatted pandas DataFrame: ")
+
+print(df)
+
+print("-"*40)
+
