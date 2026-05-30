dc2dd52bd326614157b2fd1649032dbc7c17c07b
AiswaryaRJ <aiswaryarosejacob@gmail.com>
2026-05-30 15:52:44 +0530

    Update panda.py
    create read write operations of pandas performed, created and read a csv file

--- panda.py
+++ panda.py
@@ -1,6 +1,132 @@
 import pandas as pd
 import numpy as np
-pd.DataFrame({'Bob': ['I liked it.', 'It was awful.'], 
-              'Sue': ['Pretty good.', 'Bland.']},
-             index=['Product A', 'Product B'], name='Product Reviews')
+
 pd.Series([1, 2, 3, 4, 5], index=['A','B','C','D','E'], name='Numbers')
+"""reviews
+review['colname']
+review['colname'][0]
+iloc index based selection reviews.iloc[0] row basedcfor column review.iloc[:, 0], for range of values review.iloc[:3, 0]
+loc label based selection reviews.loc['Product A'], Introduction
+Selecting specific values of a pandas DataFrame or Series to work on is an implicit step in almost any data operation you'll run, so one of the first things you need to learn in working with data in Python is how to go about selecting the data points relevant to you quickly and effectively.
+
+To start the exercise for this topic, please click here.
+
+Native accessors
+Native Python objects provide good ways of indexing data. Pandas carries all of these over, which helps make it easy to start with.
+
+Consider this DataFrame:
+
+reviews
+country	description	designation	points	price	province	region_1	region_2	taster_name	taster_twitter_handle	title	variety	winery
+0	Italy	Aromas include tropical fruit, broom, brimston...	Vulkà Bianco	87	NaN	Sicily & Sardinia	Etna	NaN	Kerin O’Keefe	@kerinokeefe	Nicosia 2013 Vulkà Bianco (Etna)	White Blend	Nicosia
+1	Portugal	This is ripe and fruity, a wine that is smooth...	Avidagos	87	15.0	Douro	NaN	NaN	Roger Voss	@vossroger	Quinta dos Avidagos 2011 Avidagos Red (Douro)	Portuguese Red	Quinta dos Avidagos
+...	...	...	...	...	...	...	...	...	...	...	...	...	...
+129969	France	A dry style of Pinot Gris, this is crisp with ...	NaN	90	32.0	Alsace	Alsace	NaN	Roger Voss	@vossroger	Domaine Marcel Deiss 2012 Pinot Gris (Alsace)	Pinot Gris	Domaine Marcel Deiss
+129970	France	Big, rich and off-dry, this is powered by inte...	Lieu-dit Harth Cuvée Caroline	90	21.0	Alsace	Alsace	NaN	Roger Voss	@vossroger	Domaine Schoffit 2012 Lieu-dit Harth Cuvée Car...	Gewürztraminer	Domaine Schoffit
+129971 rows × 13 columns
+
+In Python, we can access the property of an object by accessing it as an attribute. A book object, for example, might have a title property, which we can access by calling book.title. Columns in a pandas DataFrame work in much the same way.
+
+Hence to access the country property of reviews we can use:
+
+reviews.country
+0            Italy
+1         Portugal
+            ...   
+129969      France
+129970      France
+Name: country, Length: 129971, dtype: object
+If we have a Python dictionary, we can access its values using the indexing ([]) operator. We can do the same with columns in a DataFrame:
+
+reviews['country']
+0            Italy
+1         Portugal
+            ...   
+129969      France
+129970      France
+Name: country, Length: 129971, dtype: object
+These are the two ways of selecting a specific Series out of a DataFrame. Neither of them is more or less syntactically valid than the other, but the indexing operator [] does have the advantage that it can handle column names with reserved characters in them (e.g. if we had a country providence column, reviews.country providence wouldn't work).
+
+Doesn't a pandas Series look kind of like a fancy dictionary? It pretty much is, so it's no surprise that, to drill down to a single specific value, we need only use the indexing operator [] once more:
+
+reviews['country'][0]
+'Italy'
+Indexing in pandas
+The indexing operator and attribute selection are nice because they work just like they do in the rest of the Python ecosystem. As a novice, this makes them easy to pick up and use. However, pandas has its own accessor operators, loc and iloc. For more advanced operations, these are the ones you're supposed to be using.
+
+Index-based selection
+Pandas indexing works in one of two paradigms. The first is index-based selection: selecting data based on its numerical position in the data. iloc follows this paradigm.
+
+To select the first row of data in a DataFrame, we may use the following:
+
+reviews.iloc[0]
+country                                                    Italy
+description    Aromas include tropical fruit, broom, brimston...
+                                     ...                        
+variety                                              White Blend
+winery                                                   Nicosia
+Name: 0, Length: 13, dtype: object
+Both loc and iloc are row-first, column-second. This is the opposite of what we do in native Python, which is column-first, row-second.
+
+This means that it's marginally easier to retrieve rows, and marginally harder to get retrieve columns. To get a column with iloc, we can do the following:
+
+reviews.iloc[:, 0]
+0            Italy
+1         Portugal
+            ...   
+129969      France
+129970      France
+Name: country, Length: 129971, dtype: object
+On its own, the : operator, which also comes from native Python, means "everything". When combined with other selectors, however, it can be used to indicate a range of values. For example, to select the country column from just the first, second, and third row, we would do:
+
+reviews.iloc[:3, 0]
+0       Italy
+1    Portugal
+2          US
+Name: country, dtype: object
+Or, to select just the second and third entries, we would do:
+
+reviews.iloc[1:3, 0]
+1    Portugal
+2          US
+Name: country, dtype: object
+It's also possible to pass a list:
+
+reviews.iloc[[0, 1, 2], 0]
+0       Italy
+1    Portugal
+2          US
+Name: country, dtype: object
+Finally, it's worth knowing that negative numbers can be used in selection. This will start counting forwards from the end of the values. So for example here are the last five elements of the dataset.
+
+reviews.iloc[-5:]
+country	description	designation	points	price	province	region_1	region_2	taster_name	taster_twitter_handle	title	variety	winery
+129966	Germany	Notes of honeysuckle and cantaloupe sweeten th...	Brauneberger Juffer-Sonnenuhr Spätlese	90	28.0	Mosel	NaN	NaN	Anna Lee C. Iijima	NaN	Dr. H. Thanisch (Erben Müller-Burggraef) 2013 ...	Riesling	Dr. H. Thanisch (Erben Müller-Burggraef)
+129967	US	Citation is given as much as a decade of bottl...	NaN	90	75.0	Oregon	Oregon	Oregon Other	Paul Gregutt	@paulgwine	Citation 2004 Pinot Noir (Oregon)	Pinot Noir	Citation
+129968	France	Well-drained gravel soil gives this wine its c...	Kritt	90	30.0	Alsace	Alsace	NaN	Roger Voss	@vossroger	Domaine Gresser 2013 Kritt Gewurztraminer (Als...	Gewürztraminer	Domaine Gresser
+129969	France	A dry style of Pinot Gris, this is crisp with ...	NaN	90	32.0	Alsace	Alsace	NaN	Roger Voss	@vossroger	Domaine Marcel Deiss 2012 Pinot Gris (Alsace)	Pinot Gris	Domaine Marcel Deiss
+129970	France	Big, rich and off-dry, this is powered by inte...	Lieu-dit Harth Cuvée Caroline	90	21.0	Alsace	Alsace	NaN	Roger Voss	@vossroger	Domaine Schoffit 2012 Lieu-dit Harth Cuvée Car...	Gewürztraminer	Domaine Schoffit
+Label-based selection
+The second paradigm for attribute selection is the one followed by the loc operator: label-based selection. In this paradigm, it's the data index value, not its position, which matters.
+
+For example, to get the first entry in reviews, we would now do the following:
+
+reviews.loc[0, 'country']
+"""
+
+fruit_sales = pd.DataFrame([[35,21],[41,34]], columns=['Apples', 'Bananas'],index=['2017 Sales', '2018 Sales'] )
+print(fruit_sales)
+
+a= pd.DataFrame([['I liked it.', 'It was awful.'], ['Pretty good.', 'Bland.']], columns=['Bob', 'Sue'], index=['Product A', 'Product B'])
+print(a)
+print()
+ingredients = pd.Series(['4 cups', '1 cup', '2 large', '1 can'], index=['Flour', 'Milk', 'Eggs', 'Spam'], name='Dinner')
+print(ingredients)
+print()
+animals = pd.DataFrame({'Cows': [12, 20], 'Goats': [22, 19]}, index=['Year 1', 'Year 2'])
+print(animals)
+#reviews = pd.read_csv('winemag-data-130k-v2.csv', index_col=0) this reads a csv file and makes the first column as index column
+print()
+#to convert to a csv from a dataframe we can use to_csv method
+animals.to_csv("cows_and_goats.csv")
+pd.read_csv("cows_and_goats.csv", index_col=0)
